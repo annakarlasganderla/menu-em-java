f3e04f1c07a56d9c22476b0823362741c83b3c68
anna sganderla <sganderlakarlaanna@gmail.com>
2021-10-16 16:09:22 -0300

    ajustes

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -7,41 +7,42 @@ public class Main {
 
         Scanner leitor = new Scanner(System.in);
 
-        int opcao;
+        int opcao = 4;
 
         do {
-            System.out.println("Escolha uma das opçoes abaixo");
-            System.out.println("1 - Cadastrar pessoa");
-            System.out.println("2 - Editar cadastro");
-            System.out.println("3 - Ver pessoas cadastradas");
-            System.out.println("----------------------------");
-
-
-            System.out.print("Digite aqui sua opção: ");
-            opcao = leitor.nextInt();
+            do {
+                System.out.println("Escolha uma das opçoes abaixo");
+                System.out.println("1 - Cadastrar pessoa");
+                System.out.println("2 - Editar cadastro");
+                System.out.println("3 - Ver pessoas cadastradas");
+                System.out.println("----------------------------");
+                System.out.print("Digite aqui sua opção: ");
+                opcao = leitor.nextInt();
+            } while (opcao == 4);
 
             switch (opcao) {
                 case 1:
                     System.out.println("cadastro em andamento");
                     System.out.println("4 - voltar");
+                    opcao = leitor.nextInt();
                     break;
 
                 case 2:
                     System.out.println("modo editar em andamento");
                     System.out.println("4 - voltar");
+                    opcao = leitor.nextInt();
                     break;
 
                 case 3:
                     System.out.println("modo visualização em andamento");
-                    System.out.println("4 - voltar");
-                    break;
-
-                case 4:
+                    System.out.print("4 - voltar ");
+                    opcao = leitor.nextInt();
                     break;
 
                 default:
-                    System.out.println("isso é um teste");
+                    System.out.println("Opção inválida");
                     System.out.println("4 - voltar");
+                    opcao = leitor.nextInt();
                     break;
             }
         } while(opcao != 0);
